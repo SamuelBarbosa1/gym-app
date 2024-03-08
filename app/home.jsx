0aa7a027df21e7e565be51a60df4458da1941119
SamuelBarbosa1 <samuelbarbosa2001oliveira@gmail.com>
2024-03-08 17:44:09 -0300

    fixed sliders

--- app/home.jsx
+++ app/home.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'react-native-web'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import ImageSlider from '../components/ImageSlider';
+
 
 
 export default function home() {
@@ -10,14 +13,14 @@ export default function home() {
     <SafeAreaView className="flex-1 bg-white flex space-y-5" edges={['top']}>
       <StatusBar style="dark" />
 
-      {/* punchilne and avatar */}
+      {/* piada e avatar */}
       <View className="flex-row justify-between items-center mx-5">
         <View className="space-y-2">
             <Text
                 style={{fontSize: hp(4.5)}}
                 className="font-bold tracking-wider text-neutral-700" // COR DO "COMEÇAR A "
             >
-                COMEÇAR A 
+                PRONTO PARA
             </Text>
             <Text
                 style={{fontSize: hp(4.5)}}
@@ -31,12 +34,18 @@ export default function home() {
             style={{height: hp(6), width: hp(6)}}
             className="rounded-full" 
             />
-            <View className="bg-neutral-200 rounded-full flex justify-center items-center">
-                
+            <View className="bg-neutral-200 rounded-full flex justify-center items-center border-[3px] border-neutral-300"
+            style={{height: hp(5.5), width: hp(5.5)}}
+            >
+            <Ionicons name="notifications" size={hp(3)} color="gray" />
             </View>
         </View>
+      </View>
+
+      {/* images sliders*/}
+      <View>
+        <ImageSlider />
       </View>
     </SafeAreaView>
   )
 }
-//
