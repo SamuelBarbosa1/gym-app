975aaeb7a1ce097c278b45d192dfef83a0a0fb56
SamuelBarbosa1 <samuelbarbosa2001oliveira@gmail.com>
2024-03-08 17:19:32 -0300

    gym

---
+++ app/home.jsx
@@ -0,0 +1,42 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'react-native-web'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+
+export default function home() {
+  return (
+    <SafeAreaView className="flex-1 bg-white flex space-y-5" edges={['top']}>
+      <StatusBar style="dark" />
+
+      {/* punchilne and avatar */}
+      <View className="flex-row justify-between items-center mx-5">
+        <View className="space-y-2">
+            <Text
+                style={{fontSize: hp(4.5)}}
+                className="font-bold tracking-wider text-neutral-700" // COR DO "COMEÇAR A "
+            >
+                COMEÇAR A 
+            </Text>
+            <Text
+                style={{fontSize: hp(4.5)}}
+                className="font-bold tracking-wider text-rose-500" // deixar vermelho o "TREINAR"
+            >
+                TREINAR
+            </Text>
+        </View>
+        <View className="flex justify-center items-center space-y-2">
+            <Image source={require('../assets/images/avatar.png')}
+            style={{height: hp(6), width: hp(6)}}
+            className="rounded-full" 
+            />
+            <View className="bg-neutral-200 rounded-full flex justify-center items-center">
+                
+            </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+//
